38084f37531ee52e655bbb75db0524d02549f38f
Your Name <you@example.com>
2025-10-24 10:47:45 +0530

    Cleaned cache and rebuilt frontend

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {}, // Prevents Vite from trying to polyfill Node env
-  },
-  server: {
-    host: true,
-    port: 5173,
-  },
 });
